e02ab5840c88cb0304fe6c770aba5261ef0c3590
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2020-11-28 16:59:14 +0100

    new hotend PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -499,9 +499,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp 11.10
+    #define DEFAULT_Ki 0.63
+    #define DEFAULT_Kd 49.10
   #endif
 #endif // PIDTEMP
 
@@ -1098,7 +1098,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
